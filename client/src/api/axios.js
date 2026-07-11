ed6ed491a825167dffd530aa5e76334ca45e62b7
Pranay <kpranayk78@gmail.com>
2026-07-11 22:59:11 +0530

    Fixed url in axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://oa-wutv.onrender.com",
+  baseURL: "https://oa-cfiu7opy5-pranay9.vercel.app",
 });
 
 export default API;
